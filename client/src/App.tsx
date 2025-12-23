04beb3c81eec58d3fd74b0c02034eb134e48e061
Rinold <ris00088@students.stir.ac.uk>
2025-12-23 01:19:26 +0000

    fix: router fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -11,7 +12,7 @@ import About from "@/pages/about";
 
 function Router() {
   return (
-    <Switch>
+    <Switch location={useHashLocation()[0]}>
       <Route path="/" component={Home}/>
       <Route path="/platform" component={Platform}/>
       <Route path="/services" component={Services}/>
